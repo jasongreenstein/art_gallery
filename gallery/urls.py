b2977eecf56879000670f777bbdd9bd0c78a3ea3
Jason <jason99g@gmail.com>
2025-03-15 14:33:48 -0700

    Series List View and Art In Series List View Added

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import ArtListView
+from .views import ArtListView, SeriesListView, ArtInSeriesListView
 
 urlpatterns = [
     path("", ArtListView.as_view(), name="gallery"),
+    path("series/", SeriesListView.as_view(), name="series"),
+    path("series/<str:series>/", ArtInSeriesListView.as_view(), name="art_in_series"),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 from .models import Art
 
@@ -8,4 +8,24 @@ from .models import Art
 class ArtListView(ListView):
     model = Art
     template_name = "gallery.html"
-    gallery = Art.objects.all()
+
+
+class SeriesListView(ListView):
+    model = Art
+    # queryset = Art.objects.order_by("series").distinct("series")
+    queryset = set(Art.objects.values_list("series", flat=True))
+    template_name = "series.html"
+
+
+class ArtInSeriesListView(ListView):
+    model = Art
+    template_name = "art_in_series.html"
+
+    def get_queryset(self):
+        series = self.kwargs["series"]
+        return Art.objects.filter(series=series)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["series"] = self.kwargs["series"]
+        return context
